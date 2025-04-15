78c4302cab21f3312c10e474109a4549fb138f07
epdlrnldudnj <kje0023@naver.com>
2025-04-15 19:05:28 +0900

    project setting

---
+++ src/main/java/com/farmers/studyfit/StudyfitApplication.java
@@ -0,0 +1,13 @@
+package com.farmers.studyfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyfitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farmers/studyfit/StudyfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmers.studyfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
