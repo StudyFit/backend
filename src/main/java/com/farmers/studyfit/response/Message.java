de96518991f20a0a4e414319124cb03893caae72
epdlrnldudnj <kje0023@naver.com>
2025-05-31 22:32:59 +0900

    Feat : Response 설정

---
+++ src/main/java/com/farmers/studyfit/response/Message.java
@@ -0,0 +1,9 @@
+package com.farmers.studyfit.response;
+
+public class Message {
+    public static final String SIGNUP_SUCCESS = "회원가입 성공";
+    public static final String LOGIN_SUCCESS = "로그인 성공";
+    public static final String LOGOUT_SUCCESS = "로그아웃 성공";
+    public static final String TOKEN_SUCCESS = "토큰 재발급 성공";
+
+}
