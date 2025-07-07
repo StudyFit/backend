50f62520374bfb04a3dd6077ee2b1713c7335922
epdlrnldudnj <kje0023@naver.com>
2025-07-07 23:18:37 +0900

    Fix : global exception handler 수정함

--- src/main/java/com/farmers/studyfit/domain/member/service/MemberService.java
+++ src/main/java/com/farmers/studyfit/domain/member/service/MemberService.java
@@ -1,11 +1,11 @@
 package com.farmers.studyfit.domain.member.service;
 
-import com.farmers.studyfit.domain.connection.repository.ConnectionRepository;
 import com.farmers.studyfit.domain.member.entity.Student;
 import com.farmers.studyfit.domain.member.entity.Teacher;
 import com.farmers.studyfit.domain.member.repository.StudentRepository;
 import com.farmers.studyfit.domain.member.repository.TeacherRepository;
-import com.farmers.studyfit.exception.UserNotFoundException;
+import com.farmers.studyfit.exception.CustomException;
+import com.farmers.studyfit.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -26,7 +26,7 @@ public class MemberService {
         return teacherRepository
                 .findByLoginId(loginId)
                 .orElseThrow(() ->
-                        new UserNotFoundException("사용자를 찾을 수 없습니다.")
+                        new CustomException(ErrorCode.MEMBER_NOT_FOUND)
                 );
     }
 
@@ -38,7 +38,7 @@ public class MemberService {
         return studentRepository
                 .findByLoginId(loginId)
                 .orElseThrow(() ->
-                        new UserNotFoundException("사용자를 찾을 수 없습니다.")
+                        new CustomException(ErrorCode.MEMBER_NOT_FOUND)
                 );
     }
 }
