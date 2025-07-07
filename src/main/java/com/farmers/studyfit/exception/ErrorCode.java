5c3cd91e93a4e55467ec552d409d0428b364feaa
epdlrnldudnj <kje0023@naver.com>
2025-07-07 23:19:20 +0900

    Fix : errorcode 조금 수정함

--- src/main/java/com/farmers/studyfit/exception/ErrorCode.java
+++ src/main/java/com/farmers/studyfit/exception/ErrorCode.java
@@ -8,12 +8,11 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원을 찾을 수 없습니다."),
-
-    INVALID_FILE_TYPE(HttpStatus.BAD_REQUEST, "지원하지 않는 파일 형식입니다."),
     INVALID_REFRESH_TOCKEN(HttpStatus.BAD_REQUEST, "유효하지 않은 리프레시 토큰입니다."),
     EXPIRED_REFRESH_TOCKEN(HttpStatus.BAD_REQUEST, "리프레시 토큰이 만료되었습니다."),
     DUPLICATE_LOGIN_EXCEPTION(HttpStatus.BAD_REQUEST, "중복된 아이디 입니다."),
     INVALID_PASSWORD(HttpStatus.BAD_REQUEST,"비밀번호가 올바르지 않습니다."),
+
     CONNECTION_NOT_FOUND(HttpStatus.NOT_FOUND, "연결을 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
